48e9443bb0230bae40bbb4d019fc77979daa36af
Larissa Oliveira <52868804+lrolivera@users.noreply.github.com>
2021-01-30 15:47:43 -0300

    node organização

---
+++ Larversatil-node-basic/Front-end/src/Paginas/Produtos/Produtos.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+import Produto from '../../Componentes/card/Eletro'
+import { Container, Row } from "react-bootstrap"
+
+export default function Produtos() {
+    const [ produtos, setProdutos ] = useState([]);
+    
+    useEffect(async () => {
+        const resposta = await fetch("http://localhost:4000/produtos")
+        const dados = await resposta.json()
+        setProdutos(dados);
+    }, []);
+    return (
+        <Container>
+            <Row>
+            {produtos && produtos.map(item => <Produto dados={item} />)}
+            </Row>
+            {console.log(produtos)}
+        </Container>  
+    )
+}
